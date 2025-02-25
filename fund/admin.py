8503df41cbe5dfaa58cf4ca867c190c711a10e97
“Essam” <essamali7698@gmail.com>
2025-02-25 19:47:44 +0200

    added user and report models

--- fund/admin.py
+++ fund/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Comment,Category,Rate,Reply
+from .models import Comment,Category,Rate,Reply,User,Report
 
 admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(Rate)
 admin.site.register(Reply)
+admin.site.register(User)
+admin.site.register(Report)
 
--- fund/models.py
+++ fund/models.py
@@ -1,8 +1,27 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+import re
 
 
-class User(models.Model):
-	pass
+class User(AbstractUser):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=255)
+	mobile_phone = models.CharField(max_length=11, unique=True)
+
+	#the profile needs (pillow) installed,,, use (python -m pip install Pillow) to get pillow
+	profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True) #should change where the to save the pictures are saved
+
+	def clean(self):
+		# Custom phone number validation
+		if not re.match(r"^(010|011|012|015)\d{7}$", self.mobile_phone):
+			raise ValidationError("Phone number must be 11 digits and start with 010, 011, 012, or 015.")
+		
+	def __str__(self):
+		return f'{self.first_name} {self.last_name}'
 
 
 class Project(models.Model):
@@ -57,3 +76,20 @@ class Reply(models.Model):
 	def __str__(self):
 		return self.content 
 		# return self.written_by # هيتمسح بعد كدة دا مؤقتا بس 
+
+class Report(models.Model):
+	
+    reported_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reports")
+    
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="reported_comments")
+    
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="reports")
+    
+
+    reason = models.TextField(blank=True, null=True)  
+
+
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Report by {self.reported_by} on comment {self.comment.id}"		
