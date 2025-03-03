af55455b9c3541723aeb18168c3d17f8c072d7d3
mustafajuba98 <justjoba3@gmail.com>
2025-03-03 15:28:34 +0200

    urls for user

--- fund/urls.py
+++ fund/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProjectListView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView
+from .views import ProjectListView, ProjectCreateView, ProjectUpdateView, ProjectDeleteView, UserCreateView, UserDeleteView, UserDetailView, UserListView, UserUpdateView
 
 urlpatterns = [
 
